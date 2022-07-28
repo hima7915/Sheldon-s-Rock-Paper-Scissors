0da354b4a3f2c926e0a3c9357fb80bcaa7f0389a
Samyak Jain <88228176+samyakjain28102@users.noreply.github.com>
2022-07-28 08:58:25 +0530

    Name issue resolved

--- src/App.js
+++ src/App.js
@@ -9,14 +9,11 @@ import { useState } from 'react';
 import DSlider from './slider';
 
 const supervillains = require('supervillains');
-var user="";
-var oppo=supervillains.random();;
+var oppo=supervillains.random();
+var user='';
 function App() {
-  const [name, setName]=useState("");
-  function handleChange(event){
-    setName(event.target.value);
-    user=name;
-  };
+  const [name, setName]=useState('');
+  user=name;
   function Submit(){
     return(ReactDOM.createRoot(document.getElementById("root")).render(<Game />));
   }
@@ -24,16 +21,17 @@ function App() {
   return (
     <div className="App">
       <form onSubmit={Submit}>
-    <h1>Enter Your Name&emsp;&nbsp; <input className='name' type="text" placeholder="Type..." onChange={handleChange} value={name}></input></h1>
+    <h1>Enter Your Name&emsp;&nbsp; <input className='name' name="name" value={name} type="text" placeholder="Type..." onChange={e => setName(e.target.value)}></input></h1>
     <h1>Number of Rounds&nbsp; </h1>
     {<DSlider />}
     <div>
-    <button classsName='Multiplayer'><img src={robo} alt="Robot"></img></button> 
+    <button className='Computer'><img src={robo} alt="Robot"></img></button> 
     <button className='Multiplayer'><img src={multi} alt="Multiplayer"></img></button>
     </div> 
     </form>
     </div>
   );
+  
 }
 
 export default App;
