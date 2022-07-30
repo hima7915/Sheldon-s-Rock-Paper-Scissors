fb02bdc8c9ccde31d79dd73b334c0087703dae30
Samyak Jain <88228176+samyakjain28102@users.noreply.github.com>
2022-07-30 10:19:53 +0530

    Result page improvement

--- src/App.js
+++ src/App.js
@@ -4,36 +4,34 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import robo from "./Assets/Robot.png";
 import multi from "./Assets/Multiplayer.png";
-import Scorebar from "./scorebar";
+import Game from "./gamecomp";
 import { useState } from 'react';
 import DSlider from './slider';
 
 const supervillains = require('supervillains');
-var user="";
-var oppo=supervillains.random();;
+var oppo=supervillains.random();
+var user='';
 function App() {
-  const [name, setName]=useState("");
+  const [name, setName]=useState('');
   user=name;
-  function handleChange(event){
-    setName(event.target.value);
-  };
   function Submit(){
-    return(ReactDOM.createRoot(document.getElementById("root")).render(<Scorebar />));
+    return(ReactDOM.createRoot(document.getElementById("root")).render(<Game />));
   }
 
   return (
     <div className="App">
       <form onSubmit={Submit}>
-    <h1>Enter Your Name&emsp;&nbsp; <input className='name' type="text" placeholder="Type..." onChange={handleChange} value={name}></input></h1>
+    <h1>Enter Your Name&emsp;&nbsp; <input className='name' name="name" value={name} type="text" placeholder="Type..." onChange={e => setName(e.target.value)}></input></h1>
     <h1>Number of Rounds&nbsp; </h1>
     {<DSlider />}
-    <div className='userselection'>
+    <div>
     <button className='Computer'><img src={robo} alt="Robot"></img></button> 
     <button className='Multiplayer'><img src={multi} alt="Multiplayer"></img></button>
     </div> 
     </form>
     </div>
   );
+  
 }
 
 export default App;
